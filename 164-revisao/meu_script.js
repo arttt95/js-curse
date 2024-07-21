af0e0e45e8b06b2159b2080c0ce4330b3f33f265
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-07-21 00:41:31 -0300

    21JUL24 - Aula 164 - Revisão e desafio A-B-C.

---
+++ 164-revisao/meu_script.js
@@ -0,0 +1,24 @@
+var a = 10
+
+var b = 20
+
+var c = null //variável auxiliar utlizada na lógica
+
+document.write('A: ' + a + '<br/>')
+document.write('B: ' + b + '<br/>')
+document.write('C: ' + c + '<br/>')
+
+document.write('<hr/>')
+
+//lógica de inverdão dos valores das variáveis A e B
+
+c = a //utilizada a variável C para armazenar temporariamente o valor da variável A
+a = b //sobrepõe o valor da variável A com o valor da variável B
+b = c //sobrepóe o valor da variável B com o valor da variável C (C nesse momento contém o valor da variável A)
+c = null //opcional: remove o valor contido na variável C
+
+
+//
+document.write('A: ' + a + '<br/>')
+document.write('B: ' + b + '<br/>')
+document.write('C: ' + c + '<br/>')
