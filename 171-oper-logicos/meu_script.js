bfadb2b2219a43d77ec222a3c8f409a7c269fd65
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-07-21 18:48:29 -0300

    21JUL24 - Aula 171 - If/Else Parte 3 - Praticando com Op. Lógicos

---
+++ 171-oper-logicos/meu_script.js
@@ -0,0 +1,64 @@
+// Pipe: Ctrl + Shift -> U 007C
+
+//true && true && false = false
+if (2 == 2 && 3 >= 1 && 'a' == 'b') {
+    document.write('Verdadeiro')
+} else {
+    document.write('Falso')
+}
+
+document.write('<br/>')
+document.write('<br/>')
+
+//true || true || false = true
+if (2 == 2 || 3 >= 1 || 'a' == 'b') {
+    document.write('Verdadeiro')
+} else {
+    document.write('Falso')
+}
+
+document.write('<br/>')
+document.write('<br/>')
+
+//true || true || false = false
+if (4 == 2 || 3 >= 5 || 'a' == 'b') {
+    document.write('Verdadeiro')
+} else {
+    document.write('Falso')
+}
+
+document.write('<br/>')
+document.write('<br/>')
+
+//false = true -> operador lógico '!' inverte o resultado do teste lógico
+if (!(4 == 2)) {
+    document.write('Verdadeiro')
+} else {
+    document.write('Falso')
+}
+
+document.write('<br/>')
+document.write('<br/>')
+
+//true = false -> operador lógico '!' inverte o resultado do teste lógico
+if (!(5 >= 2)) {
+    document.write('Verdadeiro')
+} else {
+    document.write('Falso')
+}
+
+document.write('<br/>')
+document.write('<br/>')
+
+// Exercício Notas e Faltas
+
+var nota = prompt('Insira a nota do aluno:')
+var media = 7
+var faltas = prompt('Insira a quantidade de faltas do aluno:')
+var frequencia_minima = 15
+
+if (nota >= media && faltas <= frequencia_minima) {
+    document.write('O aluno foi aprovado tanto em nota quanto em frequência mínima!')
+} else {
+    document.write('O aluno não foi aprovado')
+}
