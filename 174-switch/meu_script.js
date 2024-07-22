c2ac80309703325757ff2c2d563dd174758634c8
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-07-22 01:44:33 -0300

    22JUL24 - Aula 174 - Switch (Aula teórica)

---
+++ 174-switch/meu_script.js
@@ -0,0 +1,20 @@
+// Switch
+
+// O que é o comando Switch?
+
+var opcao = 2
+
+switch(opcao) {
+
+    case 1:
+        alert('Este é o case 1')
+        break
+
+    case 2:
+        alert('Esse é o case 2')
+        break
+    
+    default:
+        alert('Este é o default')
+        break
+}
