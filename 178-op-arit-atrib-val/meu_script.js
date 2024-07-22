3fb082a2eec410eadf123233818e3fd509241822
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-07-22 04:12:22 -0300

    22JUL24 - Aula 178 - Op. Aritiméticos na atrib. de valores

---
+++ 178-op-arit-atrib-val/meu_script.js
@@ -0,0 +1,17 @@
+var teste = 10
+
+//teste = teste + 5
+//teste += 5
+//teste -= 5
+//teste *= 5
+//teste /= 2
+teste %= 3
+document.write(teste)
+
+var teste2 = 'Olá'
+
+document.write('<br/>')
+document.write('<br/>')
+
+teste2 += 'Arthur'
+document.write(teste2)
