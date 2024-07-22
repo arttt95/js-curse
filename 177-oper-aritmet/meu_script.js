57a97fbd2f28bd87ba0a6274b1abeb46bbe90e55
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-07-22 02:49:41 -0300

    22JUL24 - Aula 177 - Praticando com operadores aritiméticos

---
+++ 177-oper-aritmet/meu_script.js
@@ -0,0 +1,69 @@
+var num1 = prompt('Digite o valor de num1: ')
+var num2 = prompt('Digite o valor de num2')
+
+num1 = parseInt(num1)
+num2 = parseInt(num2)
+
+// ADIÇÂO
+document.write('A soma entre ' + num1 + ' e ' + num2 + ' é: ' + (num1 + num2))
+
+document.write('<br/>')
+document.write('<br/>')
+
+// SUBTRAÇÂO
+document.write('A subtração entre ' + num1 + ' e ' + num2 + ' é: ' + (num1 - num2))
+
+
+document.write('<br/>')
+document.write('<br/>')
+
+// MULTIPLICAÇÂO
+document.write('A multiplicação entre ' + num1 + ' e ' + num2 + ' é: ' + (num1 * num2))
+
+document.write('<br/>')
+document.write('<br/>')
+
+// DIVISÂO
+document.write('A divisão entre ' + num1 + ' e ' + num2 + ' é: ' + (num1 / num2))
+
+document.write('<br/>')
+document.write('<br/>')
+
+// MÓDULO
+document.write('O módulo entre ' + num1 + ' e ' + num2 + ' é: ' + (num1 % num2))
+
+document.write('<br/>')
+document.write('<br/>')
+
+// INCREMENTO PRÉ
+document.write('O incremento pré de ' + num1 + ' é: ' + (++num1))
+
+document.write('<br/>')
+document.write('<br/>')
+
+// INCRVEMENTO PÓS
+document.write('O incremento pós de ' + num1 + ' é: ' + (num1++))
+
+document.write('<br/>')
+document.write('<br/>')
+
+// DEMONSTRANDO O INCREMENTO PÓS
+document.write('O valor de "num1" foi alterado após a instrução de comando, ficando: ' + num1)
+
+document.write('<br/>')
+document.write('<br/>')
+
+// DECREMENTO PRÉ
+document.write('O decremento pré de ' + num1 + ' é: ' + (--num1))
+
+document.write('<br/>')
+document.write('<br/>')
+
+// DECREMENTO PÓS
+document.write('O decremento pós de ' + num1 + ' é: ' + (num1--))
+
+document.write('<br/>')
+document.write('<br/>')
+
+// DEMONSTRANDO O DECREMENTO PÓS
+document.write('O valor de "num1" foi alterado após a instrução de comando, ficando: ' + num1)
