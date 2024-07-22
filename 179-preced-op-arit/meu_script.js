979292216ebedcfe7e86f7f88ae77807ce3e98a5
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-07-22 04:26:22 -0300

    22JUL24 - Aula 179 - Preced. aritim. na exec. de Op. Aritim.

---
+++ 179-preced-op-arit/meu_script.js
@@ -0,0 +1,23 @@
+document.write(11 + 5 - 5 * 2 / 2)
+
+//1º Multiplicação e Divisão
+//2º Soma e Subtração
+
+//Ficaria assim por etapas
+//1ª Etapa resolveria a multiplicação por estar mais a esquerda do que a divisão
+//document.write(11 + 5 - 5 * 2 / 2)
+//document.write(11 + 5 - 10 / 2)
+
+//2ª Etapa resolveria a divisão
+//document.write(11 + 5 - 10 / 2)
+//document.write(11 + 5 - 5)
+
+//3ª Etapa resolveria a soma por estar mais a esquerda do que a subtração
+//document.write(11 + 5 - 5)
+//document.write(16 - 5)
+
+//4ª Etapa e por último resolveria a subtração ficando com resultado 11
+//document.write(16 - 5)
+//document.write(11)
+
+//lembrando sempre que '()' sempre tem precedência saindo da resolução convêncional
