01c77c37349407a5a7996b45a5c49ad87b6afd9a
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-07-23 00:31:59 -0300

    23JUL24 - Aula 188 - Funções para tarefas matemáticas

---
+++ 188-func-nat-matem/meu_script.js
@@ -0,0 +1,25 @@
+//ceil arredonda sempre para o valor inteiro acima do existente
+var x = Math.ceil(10.380)
+
+document.write(x)
+
+document.write('<br/><br/>')
+
+//floor desconsidera o conteúdo fracionado e devolve apenas a parte inteira do valor
+var y = Math.floor(10.380)
+
+document.write(y)
+
+document.write('<br/><br/>')
+
+//round leva em consideração o peso da fração e arredonda pra cima ou pra baixo
+var z = Math.round(10.480)
+
+document.write(z)
+
+document.write('<br/><br/>')
+
+//devolve um valor randomico entre 0 e 1
+var a = Math.random()
+
+document.write(a)
