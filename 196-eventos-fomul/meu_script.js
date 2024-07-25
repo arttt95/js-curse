24fc2874e97ed115509e25284f5e70cea94cbb1a
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-07-25 19:37:14 -0300

    25JUL24 - Aula 196 - Eventos Formulários

---
+++ 196-eventos-fomul/meu_script.js
@@ -0,0 +1,11 @@
+function acao1 () {
+    console.log('Evento onfocus disparado')
+}
+
+function acao2 () {
+    console.log('Evento onblur disparado')
+}
+
+function acao3 () {
+    console.log('Evento onchange disparado')
+}
