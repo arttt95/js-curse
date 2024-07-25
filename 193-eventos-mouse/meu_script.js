80e6720a1ba8ed18003a45af41c948182fe434f6
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-07-25 03:01:18 -0300

    25JUL24 - Aula 193 - Eventos MOUSE

---
+++ 193-eventos-mouse/meu_script.js
@@ -0,0 +1,19 @@
+function acao () {
+    alert('Click único disparado')
+}
+
+function acao2 () {
+    alert('Double click disparado')
+}
+
+function acao3 () {
+    alert('Mouse liberado na area')
+}
+
+function acao4 () {
+    alert('Mouse sobrepos a região')
+}
+
+function acao5 () {
+    alert('Mouse saiu da região do elemento')
+}
