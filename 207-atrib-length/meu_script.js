0b06e78077ef65eb5d79a1658c0977a3b4500cb2
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-07-28 23:56:50 -0300

    28JUL24 - Aula 207 - Extra (Diferença entre o atributo lenght)

---
+++ 207-atrib-length/meu_script.js
@@ -0,0 +1,10 @@
+var lista_frutas = ['Banana', 'Maçã', 'Morango', 'Uva']
+
+var lista_frutas2 = Array()
+
+lista_frutas2[5] = 'Banana'
+lista_frutas2[6] = 'Maçã'
+lista_frutas2[7] = 'Morango'
+lista_frutas2[8] = 'Uva' 
+
+console.log(lista_frutas2)
