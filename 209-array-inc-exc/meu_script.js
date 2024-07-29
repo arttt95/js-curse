7d4966027580c48cbc2d583d767c3527917f868f
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-07-29 01:12:07 -0300

    29JUL24 - Aula 209 - Array (Métodos de inclusão e exclusão de elementos)

---
+++ 209-array-inc-exc/meu_script.js
@@ -0,0 +1,22 @@
+var lista_coisas = Array()
+
+lista_coisas['frutas'] = Array()
+lista_coisas['pessoas'] = []
+
+lista_coisas['frutas'].push('Banana')
+lista_coisas['frutas'].unshift('Uva')
+
+//incluir elemento no final do array
+//lista_frutas.push('Uva')
+
+//incluir elemento no início do array
+//lista_frutas.unshift('Morango')
+
+//excluir um elemento no final do array
+//lista_frutas.pop()
+
+//excluir um elemento no início do array
+//lista_frutas.shift()
+
+
+console.log(lista_coisas)
