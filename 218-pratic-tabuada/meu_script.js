24c6719e63beef19693a7d4857ef8b8dbacc0ddf
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-07-31 01:22:02 -0300

    31JUL24 - Aula 218 - Praticando Loops (Tabuada)

---
+++ 218-pratic-tabuada/meu_script.js
@@ -0,0 +1,11 @@
+for (var y = 1; y <= 10; y++) {
+
+    for (var x = 1; x <= 10; x++) {
+
+        document.write(y + ' x ' + x + ' = ' + (y * x))
+        document.write('<br>')
+    }
+
+    document.write('<hr>')
+
+}
