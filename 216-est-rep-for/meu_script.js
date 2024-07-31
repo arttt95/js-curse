724e3f4d0c14b97cd56b0205daefdd66f5028e34
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-07-30 23:15:19 -0300

    30JUL24 - Aula 216 - Esrtruturas de repetição (For)

---
+++ 216-est-rep-for/meu_script.js
@@ -0,0 +1,21 @@
+/*
+var x = 1
+
+while (condição) {
+
+}
+
+var x = 1
+do {
+
+} while (condição)
+*/
+
+for (var x = 10; x >= 0; x--) {
+
+    document.write(x + '<br>')
+
+    //break
+    //continue
+
+}
