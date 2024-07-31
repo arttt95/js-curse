8dfeef8e588d88c8dae3890c1e97747430b5a338
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-07-31 00:39:02 -0300

    30JUL24 - Aula 217 - Percorrendo e listando itens de Arrays

---
+++ 217-percorrer-list/meu_script.js
@@ -0,0 +1,15 @@
+var lista_frutas = ['Banana', 'Maçã', 'Morango', 'Uva']
+
+var y = 0
+
+console.log(lista_frutas.length)
+
+
+while(y < lista_frutas.length) {
+
+    document.write(lista_frutas[y])
+    document.write('<br>')
+    //ação
+    y++
+
+}
