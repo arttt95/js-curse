3791e4b70ddf1dec6df43541f563fb02e98fb2c9
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-07-30 23:07:24 -0300

    30JUL24 - Aula 215 - Estruturs de repetião (Do While)

---
+++ 215-est-rep-do-whi/meu_script.js
@@ -0,0 +1,18 @@
+var x = 11
+
+do {
+
+    document.write(x + '<br>')
+
+    //break
+    //continue
+
+} while (x <= 10)
+
+/*
+while(false) {
+
+    document.write(x + '<br>') //Este trecho seria ignorado
+
+}
+*/
