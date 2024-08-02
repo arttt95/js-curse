ee341bb4de030e3dab6cf666eb0600c0ffe44346
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-08-01 22:04:32 -0300

    01AGO24 - Aula 226 - (Screen)

---
+++ 226-bom-screen/meu_script.js
@@ -0,0 +1,18 @@
+var altura = window.screen.availHeight
+
+var largura = window.screen.availWidth
+
+//document.write('A altura do navegador é: ' + altura)
+
+//document.write('<hr>')
+
+//document.write('A largura do navegador é: ' + largura)
+
+if (largura < 500) {
+
+    document.write('Lógica para a  impressão do menu versão mobile')
+
+} else {
+    
+    document.write('Lógica para a impressão do menu versão web ')
+}
